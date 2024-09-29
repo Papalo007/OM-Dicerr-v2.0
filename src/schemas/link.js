3370bc5205f133d0a52d81e3fed88f1ab6c1f97c
Papalo007 <ok.papalo007@gmail.com>
2024-09-29 21:08:32 +0300

    edited gitignore

--- src/schemas/link.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-const linkSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  userID: String,
-  riotID: String,
-  status: String,
-});
-
-module.exports = model("Link", linkSchema, "riotdiscordlink");
