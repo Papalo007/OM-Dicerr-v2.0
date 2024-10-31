ac953d9f62c57e770156ee08764669d9785c4957
Papalo007 <ok.papalo007@gmail.com>
2024-10-31 14:28:30 +0200

    did progress on entitlementCreate

--- src/events/client/entitlementCreate.js
+++ src/events/client/entitlementCreate.js
@@ -1,15 +1,47 @@
+const fetch = require('node-fetch');
+const { token } = process.env;
+
 module.exports = {
   name: "entitlementCreate",
   async execute(entitlement, client) {
     const dev = await client.users.fetch("706547981184270428");
-
-    
-    console.log(`${entitlement}\n\n${entitlement.skuId}`);
-    
+    const user = await client.users.fetch(entitlement.userId);
     try {
-      await dev.send(entitlement);
+      const response = await fetch(
+        `https://discord.com/api/v10/store/skus/${entitlement.skuId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bot ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok)
+        throw new Error(`Failed to fetch SKU details: ${response.statusText}`);
+
+      const skuData = await response.json();
+
+      console.log("SKU Details:", skuData);
+      console.log("SKU Name:", skuData.name);
+
     } catch (error) {
-      await dev.send(entitlement.toString());
+      console.error("Error fetching SKU details:", error);
     }
+
+    const message = `# ----------------------\n# Purchase complete!\nSKU Name: ${skuData.name}, SKU ID: ${entitlement.skuId}
+Starts at: ${entitlement.startsAt}, Ends at: ${entitlement.endsAt}\nConsumed: ${entitlement.consumed}, Entitlement ID: ${entitlement.id}
+User's tag: ${user.tag}, ID: ${entitlement.userId}`;
+
+    if (entitlement.isGuildSubscription) {
+      message += "\nGuild Subscription";
+    } else if (entitlement.isUserSubscription) {
+      message += "\nUser Subscription";
+    }
+
+    await dev.send(message);
+
+    //TODO: Add a switch statement to send the user who bought this thing a message + their privileges.
   },
 };
